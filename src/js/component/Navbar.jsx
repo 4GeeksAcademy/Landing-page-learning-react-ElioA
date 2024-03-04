8cd9bd0e91989d7497168a5a5893139d70ec7059
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-03-04 18:36:30 +0000

    my first commit

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 
 
+const navlinks=["Home", "About", "Services", "Contact"]
+
+const renderNavLinks = () => {
+
+  return navlinks.map((navitem)=>{
+    return (  <li>
+      <a className="nav-link" href="#">{navitem}</a>
+    </li>)
+  })
+
+}
+
 const Navbar=()=>{
 
     return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
- <div className="container-fluid">
-    <a className="navbar-brand" href="#">Start Bootstrat</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-      <ul className="navbar-nav ml-auto">
-        <li>
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Service</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-      </ul> 
-    </div>
-    </div>
-  </nav>
+            <div className="container-fluid">
+               <a className="navbar-brand" href="#">Start Bootstrap</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+               </button>
+              <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                 <ul className="navbar-nav ml-auto">
+                    {renderNavLinks()}
+                  </ul> 
+              </div>
+            </div>
+           </nav>
   
 
 }
